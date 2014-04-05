c4147050484b3c1123b25ae50d8d2a8a93e184da
Ayberk Tosun <ayberk.tosun@gmail.com>
2014-04-05 23:01:46 +0300

    Display sample matrix

--- view.py
+++ view.py
@@ -1,29 +1,37 @@
 from Tkinter import *
 
+global layoutMatrix
+def layoutMatrix(matrix, rects, w):
+    for i in range(4):
+        for j in range(4):
+            current_rect = rects[i][j]
+            w.create_text(current_rect[0], current_rect[1], text=str(matrix[i][j]))
 
-def initUI():
+def initUI(matrix):
     master = Tk()
     master.title("2048")
     SIDE = 100
     w = Canvas(master, width=SIDE * 4, height=SIDE * 4)
     master.title = "2048"
     w.pack()
-
-
-    # w.create_rectangle(0, 0, 100, 100)
-    # w.create_rectangle(100, 0, 200, 100)
-    # w.create_rectangle(200, 0, 300, 100)
-    # w.create_rectangle(300, 0, 400, 100)
-
+    
+    rects = []
     for i in range(4):
+        row = []
         for j in range(4):
-            rect = w.create_rectangle(SIDE * j, 0, SIDE * (j + 1), SIDE * (i + 1))
-            w.create_text(SIDE * i + 50, SIDE * j + 50, text=str(i + 1) + ", " + str(j + 1))
+            w.create_rectangle(SIDE * j, 0, SIDE * (j + 1), SIDE * (i + 1))
+            # w.create_text(SIDE * i + 50, SIDE * j + 50, text=str(i + 1) + ", " + str(j + 1))
+            row.append([SIDE * i + 50, SIDE * j + 50])
+        rects.append(row)
+
+    global layoutMatrix
+    layoutMatrix(matrix, rects, w)
 
 
-# def layoutMatrix(matrix):
-#     for row in matrix:
-#         for cell in row:
+matrix = [[0, 0, 0, 0],
+          [0, 0, 2, 2],
+          [0, 0, 0, 0],
+          [4, 0, 0, 0]]
 
-initUI()
+initUI(matrix)
 mainloop()
