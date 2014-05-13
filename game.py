cde7ffdc912704f82b7a18ec5842225852def81b
Ayberk Tosun <ayberk.tosun@gmail.com>
2014-05-13 16:44:32 +0300

    Add docstrings

--- game.py
+++ game.py
@@ -15,21 +15,25 @@ class Grid(object):
         print "Play with WASD!"
 
     def begin(self):
+        '''Start the game.'''
         self.grid_view = GridView(self)
         self.grid_view.initUI(self.matrix)
         self.grid_view.mainloop()
 
     def get_column(self, nth):
+        '''Get column at index.'''
         column = []
         for row in self.matrix:
             column.append(row[nth])
         return column
 
     def set_column(self, nth, column):
+        '''Replace a column at index "nth".'''
         for i in range(4):
             self.matrix[i][nth] = column[i]
 
     def insert_random_num(self):
+        '''Insert a random number to the grid.'''
         x = randint(0, 3)
         y = randint(0, 3)
 
@@ -40,7 +44,10 @@ class Grid(object):
         self.matrix[y][x] = 2
 
     def slide(self, direction):
-        ''' u for up, r for right, l for left, d for down '''
+        ''' Apply the corresponding shift to a column or row.
+        Columns are treated as rows thus sliding a row up is
+        same as shifting it left. 
+        u for up, r for right, l for left, d for down '''
         if direction == "u":
             for i in range(4):
                 column = self.get_column(i)
@@ -67,7 +74,8 @@ class Grid(object):
         self.grid_view.layoutMatrix(self.matrix)
 
     def shift_left(self, array):
-        # If empty no shift happens
+        '''Shift a list left. If the input list is [2, 2, 4, 8]
+        it would be equal to [4, 4, 8] after a left-shift is applied.'''
         if sum(array) == 0:
             return array
 
@@ -85,13 +93,16 @@ class Grid(object):
         return array
 
     def shift_right(self, array):
-        # If left shift is applied to the reversed
-        # array and then the output is reversed again
-        # we get a right shift.
+        '''If left shift is applied to the reversed
+        array and then the output is reversed again
+        we get a right shift.'''
         array = self.shift_left(array[::-1])
         return array[::-1]
 
     def matrix_str(self):
+        '''Create a string representation of the matrix
+        in the current state. This method is to be used
+        for debugging purposes.'''
         matrix_str = ""
         for row in self.matrix:
             row_str = ""
